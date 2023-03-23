3ef92415cd630938821c5612bc5fb19a04a5fbeb
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-03-22 21:31:52 -0400

    build: instalação do tailwind e inicio do refactor

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    colors:{
+      'primary-color': '#01b5c0',
+      'primary-color-400': '#0BA8B1',
+      'secondary-color': '#d6f3f5',
+      'white': "#fff",
+      'emphasis': '#9b9191'
+    },
+    fontSize: {
+      'title-1': ['68px', '74px'],
+      'title-2': ['48px', '62px'],
+      'title-3': ['36px', '46.8px'],
+      'title-4': ['28px', '36.4px'],
+      'title-5': ['16px', '20.8px']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
