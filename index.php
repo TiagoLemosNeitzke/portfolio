6e1cf9c389cbf0d9af401a4d7dad61e07861645c
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-03-25 15:53:03 -0400

    feat: download do curricullum

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once 'vendor/autoload.php';
 include './src/html/base.html';
 include './src/html/nav.html';
 include './src/html/principal.html';
-include './src/html/aboutMe.php';
+include './src/html/aboutMe.html';
 include './src/html/services.html';
 include './src/html/footer.html';
