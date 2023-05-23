9c4543d2b4ed2b5881c5024e53dafec69747cfd3
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-05-23 17:20:14 -0400

    ajustes de layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/html/*.{html,js}", "./src/php/*.{php}"],
+  content: ["./src/html/*.{html,js}", "./src/php/*.php"],
   theme: {
     colors: {
       'primary-color': '#290535',
